ac4ab6a95e2e17bf8d5cf15bc45b0df63ff6f207
Berthouzoz Michaël <hello@michael-berthouzoz.me>
2016-04-13 13:34:29 +0200

    init commit

---
+++ src/drum.erl
@@ -0,0 +1,39 @@
+%%%-------------------------------------------------------------------
+%%% @author Michaël
+%%% @copyright (C) 2016, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 13. avr. 2016 10:39
+%%%-------------------------------------------------------------------
+-module(drum).
+-author("Michaël").
+
+%% API
+-export([render_file/1, decode_file/1, render_tracks/2, parse_measure/1, parse_header/1, binary_to_string/1, parse_tracks/1, render/3]).
+
+
+render_file(_Arg0) ->
+    erlang:error(not_implemented).
+
+decode_file(_Arg0) ->
+    erlang:error(not_implemented).
+
+render_tracks(_A, _B) ->
+    erlang:error(not_implemented).
+
+
+parse_measure(_A) ->
+    erlang:error(not_implemented).
+
+parse_header(_Arg0) ->
+    erlang:error(not_implemented).
+
+binary_to_string(_Arg0) ->
+    erlang:error(not_implemented).
+
+parse_tracks(_Arg0) ->
+    erlang:error(not_implemented).
+
+render(_Arg0, _Arg1, _Arg2) ->
+    erlang:error(not_implemented).
